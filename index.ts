3b237719aa8eb7dbd062b32cca653b2e0202b1b2
udeshikanimali <udeshikanimali644@gmail.com>
2024-08-19 15:37:57 +0530

    changes

--- index.ts
+++ index.ts
@@ -124,14 +124,15 @@
 
 
 /// <reference path="global.d.ts" />
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 
 import express from "express";
 import bodyParser from "body-parser";
 import fs from "fs";
 import https from "https";
 import morgan from "morgan";
-import { initRoutes } from "./src/routes/in"; // Correct import of the initRoutes function
+import { initRoutes } from "./src/routes/in";
 import { logRequest } from "./src/middleware/request-logger";
 import { handleError } from "./src/middleware/error-handler";
 import { Authentication } from "./src/middleware/authentication";
@@ -141,14 +142,14 @@ import cors from "cors";
 import { verifyRole } from "./src/middleware/verify-role";
 import { Role } from "./src/models/user-model";
 import { Server } from "socket.io";
-import { DefaultEventsMap } from "socket.io/dist/typed-events";
 import * as http from "http";
 
 const production = process.env.NODE_ENV === "production";
-const PORT: any = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
 databaseSetup();
 const app = express();
+
 app.use("/webhook", express.raw({ type: "application/json" }));
 app.use(logRequest);
 app.use(express.json());
@@ -160,10 +161,13 @@ app.use(morgan("combined"));
 if (!production) {
   app.use(
     cors({
-      optionsSuccessStatus: 200,
       origin: "*",
       allowedHeaders: [
-        "Content-Type, Access-Control-Allow-Headers, Access-Control-Allow-Origin, Authorization, X-Requested-With",
+        "Content-Type",
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Origin",
+        "Authorization",
+        "X-Requested-With",
         "Cache-Control",
       ],
     })
@@ -188,41 +192,39 @@ if (production) {
   server = http.createServer(app);
 }
 
-export const io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any> = new Server(server, {
+const io = new Server(server, {
   cors: {
     origin: "*",
     methods: ["PUT", "GET", "POST", "DELETE", "OPTIONS"],
     credentials: true,
   },
-  transports: ["polling", "websocket"]
+  transports: ["polling", "websocket"],
 });
 
-io.on("connection", (socket: any) => {
+io.on("connection", (socket) => {
   console.log("A user connected");
 
   if (socket) {
     io.to(socket.id).emit("new-connect", { socket_id: socket.id });
-    console.log('========new connect=================')
+    console.log("========new connect=================");
   }
 
-  socket.on("sendMessage", (data:any) => {
+  socket.on("sendMessage", (data) => {
     const { recipientSocketId, message } = data;
     io.to(recipientSocketId).emit("newMessage", { message });
-    console.log('========new send message emit=================')
+    console.log("========new send message emit=================");
   });
 
   socket.on("disconnect", () => {
     console.log("User disconnected");
   });
-
-  // Handle other socket events as needed
 });
 
 server.listen(PORT, () => {
-  console.log("--> Server successfully started at port :: " + PORT);
+  console.log(`--> Server successfully started at port :: ${PORT}`);
 });
 
-initRoutes(app); // Initialize routes correctly
+initRoutes(app);
 app.use(handleError);
 
 export default app;
